55949143cc8e92d5c79cc5bad4a215d18b157e20
chlsky <120708147+chlsky@users.noreply.github.com>
2022-12-16 16:45:38 +0900

    Update and rename index.js to name.js

--- index.js
+++
@@ -1,4 +0,0 @@
-var number1 = prompt('숫자 1 입력','메세지를 입력해주세요')
-var number2 = prompt('숫자 2 입력','메세지를 입력해주세요')
-
-document.write(number1 + number2 )
---
+++ name.js
@@ -0,0 +1,4 @@
+const number1 = prompt('숫자 1 입력','메세지를 입력해주세요')
+const number2 = prompt('숫자 2 입력','메세지를 입력해주세요')
+
+document.write(number1 + number2 )
